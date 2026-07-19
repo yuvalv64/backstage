6db584a7890930116e96d9aa001df9275e5f5438
YUVAL VIZEL <yuvalvizpr@gmail.com>
2026-07-19 16:35:44 +0300

    adds python app with k8s deployments

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+from flask import Flask
+import datetime 
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def info():
+    return jsonify({
+        "service":"backstage",
+        "version":"1.0.0",
+        "time": datetime.datetime.now().isoformat(),
+        "hostname": socket.gethostname(),
+        "deployed_on": "K8S",
+        "message":"Welcome to the backstage service!"
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({
+        "status":"healthy"
+    })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
