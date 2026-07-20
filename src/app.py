9afcddace0bb1346c35c539e4eb8c34a59d1c1ac
YUVAL VIZEL <yuvalvizpr@gmail.com>
2026-07-20 12:20:33 +0300

    cd development

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def info():
     return jsonify({
         "service":"backstage",
-        "version":"2.0.0",
+        "version":"3.0.0",
         "time": datetime.datetime.now().isoformat(),
         "hostname": socket.gethostname(),
         "deployed_on": "K8S",
